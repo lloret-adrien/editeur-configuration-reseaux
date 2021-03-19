7d4f23db129e8aad46e68f4e90ed6b077bcad54b
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-03-19 06:47:42 +0100

    Ajout test Composant

---
+++ projetS4/src/test/java/cheron_ezzaamari_soysal/model/ComposantTest.java
@@ -0,0 +1,31 @@
+package cheron_ezzaamari_soysal.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.junit.jupiter.api.Test;
+
+class ComposantTest {
+
+    @Test
+    void test_composant_avecConcurrance() throws InterruptedException {
+        final int NOMBRE_DE_THREADS = 10;
+        final int NOMBRE_DE_COMPOSANTS = 100;
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        CountDownLatch latch = new CountDownLatch(NOMBRE_DE_THREADS);
+        Runnable creer100Composants = () -> {
+            for (int i = 0; i < NOMBRE_DE_COMPOSANTS; i++) {
+                new Composant("Switch");
+                latch.countDown();
+            }
+        };
+        for (int i = 0; i < NOMBRE_DE_THREADS; i++)
+            service.execute(creer100Composants);
+        latch.await();
+        assertEquals(NOMBRE_DE_THREADS * 100, new Composant("Switch").getId());
+    }
+
+}
